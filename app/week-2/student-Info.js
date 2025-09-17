1b7da5a9ec27493c98e68302ae29974582204bfe
Tushardawar <Tushar.Dawar@edu.sait.ca>
2025-09-17 11:00:43 -0600

    Create student-Info.js

---
+++ app/week-2/student-Info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Name - Tushar Dawar</h1>
+      <h3>
+        GitHub Link
+        <Link href="https://github.com/tushardawar/cprg306-assignments"></Link>
+      </h3>
+    </main>
+  );
+}
+
