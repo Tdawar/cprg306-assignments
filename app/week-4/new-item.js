b0925df4334c2f58d07bb22f508be50c4ec2796d
Tushardawar <tushardawar446@gmail.com>
2025-10-02 16:13:18 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,5 +1,43 @@
-export default function NewItem(){
-  return(
-    {}
-  );
+"use client"
+
+import { useState } from "react";
+
+export default function NewItem() {
+
+  const increment = () => {
+    if (quantity < 20) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
 }
+// Conditional styling
+let buttonStylesInc = "mt-4 bg-blue-500 hover:bg-blue-300 text-white px-4 ml-5 py-2 rounded";
+if (count >= 20) {
+  buttonStylesInc += " opacity-50 cursor-not-allowed";
+}
+let buttonStylesDec = "mt-4 bg-blue-500 hover:bg-blue-300 text-white px-4 ml-5 py-2 rounded";
+if (count <= 1) {
+  buttonStylesDec += " opacity-50 cursor-not-allowed";
+}
+
+return (
+  <main className="p-10">
+    <p>Current Count: {count}
+      <button
+        className={buttonStylesDec}
+        onClick={decrement}
+      >-</button>
+      <button
+        className={buttonStylesInc}
+        onClick={increment}
+      >+</button>
+    </p>
+  </main>
+)
