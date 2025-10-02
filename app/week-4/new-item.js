f38263eb1521d854f44a4a5d80444f8d515e7726
Tushardawar <tushardawar446@gmail.com>
2025-10-02 16:36:43 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 export default function NewItem() {
 
+  const [quantity, setQuantity] = useState(1)
+
   const increment = () => {
     if (quantity < 20) {
       setQuantity(quantity + 1);
@@ -16,28 +18,28 @@ export default function NewItem() {
     }
   };
 
-}
-// Conditional styling
-let buttonStylesInc = "mt-4 bg-blue-500 hover:bg-blue-300 text-white px-4 ml-5 py-2 rounded";
-if (count >= 20) {
-  buttonStylesInc += " opacity-50 cursor-not-allowed";
-}
-let buttonStylesDec = "mt-4 bg-blue-500 hover:bg-blue-300 text-white px-4 ml-5 py-2 rounded";
-if (count <= 1) {
-  buttonStylesDec += " opacity-50 cursor-not-allowed";
-}
+  // Conditional styling
+  let buttonStylesInc = "mt-4 bg-blue-500 hover:bg-blue-300 text-white px-4 ml-5 py-2 rounded";
+  if (quantity >= 20) {
+    buttonStylesInc += " opacity-50 cursor-not-allowed";
+  }
+  let buttonStylesDec = "mt-4 bg-blue-500 hover:bg-blue-300 text-white px-4 ml-5 py-2 rounded";
+  if (quantity <= 1) {
+    buttonStylesDec += " opacity-50 cursor-not-allowed";
+  }
 
-return (
-  <main className="p-10">
-    <p>Current Count: {count}
-      <button
-        className={buttonStylesDec}
-        onClick={decrement}
-      >-</button>
-      <button
-        className={buttonStylesInc}
-        onClick={increment}
-      >+</button>
-    </p>
-  </main>
-)
+  return (
+    <main className="p-10">
+      <p>Current quantity: {quantity}
+        <button
+          className={buttonStylesDec}
+          onClick={decrement}
+        >-</button>
+        <button
+          className={buttonStylesInc}
+          onClick={increment}
+        >+</button>
+      </p>
+    </main>
+  );
+}
